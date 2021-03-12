ae3d117355876767d3efa7faa9d6567233b8de2c
templatetuners <adrian.kaiax@gmail.com>
2021-03-12 10:31:46 +0200

    fixes

--- src/android/tscprintwifi.java
+++ src/android/tscprintwifi.java
@@ -14,9 +14,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+
+import android.app.Activity;
+
+import android.os.Environment;
 
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -25,62 +27,57 @@ import android.net.Uri;
 import android.util.Log;
 
 import com.example.tscdll.TscWifiActivity;
-//import com.example.tscdll.TSCActivity;
+import com.example.tscdll.TSCActivity;
 
 public class tscprintwifi extends CordovaPlugin {
 	
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
-        // Create an instance( LabelPrinter class )
-        //printer = new LabelPrinter();
+
+		//Activity activity = cordova.getActivity();
+    	//String packageName = activity.getPackageName();
     }
 
-	public void printBmp(String printIp,int printPort,String path){
+	public void printBmp(String printIp, String path){
 		//https://www.freesion.com/article/54671081479/
+
 		try{
 			//声明以太网连接实例
 			//if(TscEthernetDll==null){
 				//TscEthernetDll = new TscWifiActivity();
 			//}
+			
 			TscWifiActivity TscEthernetDll = new TscWifiActivity();
-			//打开指定IP和端口号
-			TscEthernetDll.openport(printIp, printPort);
 
-			TscEthernetDll.downloadbmp(path);
-			
-			//String cacheDir = getCacheDir().getAbsolutePath();
+			TscEthernetDll.openport(printIp, 9100);
 
-			//Toast.makeText(this.cordova.getActivity(), cacheDir, Toast.LENGTH_LONG).show();
-			
-			//设置标签的寬度、高度、列印速度、列印浓度、感应器类别、gap/black mark 垂直间距、gap/black mark 偏移距离)
-			TscEthernetDll.setup(100, 150, 4, 4, 0, 0, 0);
+			//TscEthernetDll.downloadbmp(path);
+												 //1
+			TscEthernetDll.setup(100, 150, 3, 15, 0, 0, 0);
+			//setup(int width, int height, int speed, int density, int sensor, int sensor_distance, int sensor_offset)
 
 			TscEthernetDll.clearbuffer();
 
-			//打印二维码的参数和内容
-			//String command = "QRCODE 120,90,Q,8,A,0,M1,S7,\"" + qrCode+"\"";
-			//传送指令
-			//TscEthernetDll.sendcommand(command);
-			//图片路径
-			//String path = "/sdcard/Download/" + "qrPic" + ".png";
-			//设置图片在标签纸的坐标
-			//TscEthernetDll.sendpicture(0,20,path);
+			// x, y, path
+			TscEthernetDll.sendpicture(0, 0, path);
 
-			//TscEthernetDll.sendcommand("PUTBMP 100,520,\"path\"\n");
-
-			TscEthernetDll.sendcommand("PUTBMP 0,0,\""+path+", 8\"\n");
+			//TscEthernetDll.sendcommand("GAP 0,0\r\n");
+			//TscEthernetDll.sendcommand("CLS\r\n");
+			//TscEthernetDll.sendcommand("PUTBMP 100,520,\""+path+"\"\r\n");
 
 			//延迟一秒等待装载完了再打印
 			Thread.sleep(1000);
+			TscEthernetDll.sendcommand("PRINT 1\r\n");
 
+			//String status = TscEthernetDll.printerstatus();
 
-			/*	說明: 列印标签內容
-			參數:
-			a: 字串型別，设定列印标签式数(set)
-			b: 字串型別，设定列印标签份数(copy)*/
-			TscEthernetDll.printlabel(1, 1);
+			//TscEthernetDll.printlabel(1, 1);
+			// quantity / copy
+			
 			TscEthernetDll.closeport();
-			Toast.makeText(this.cordova.getActivity(), path, Toast.LENGTH_LONG).show();
+			
+			Toast.makeText(this.cordova.getActivity(), "Printed", Toast.LENGTH_LONG).show();
+
 		} catch(Exception e){
 			//Log.e(TAG, "打印异常->" + e.getMessage());
 			//callbackContext.error("Error encountered: " + e.getMessage());
@@ -109,56 +106,14 @@ public class tscprintwifi extends CordovaPlugin {
         }
 
 		if(action.equals("print")) {
-/*
-			TscWifiActivity TscEthernetDll = new TscWifiActivity();
-			TscEthernetDll.openport("192.168.221.12", 9100);
-			TscEthernetDll.sendcommand("SIZE 3,1\r\n");
-			TscEthernetDll.sendcommand("GAP 0,0\r\n");
-			TscEthernetDll.sendcommand("CLS\r\n");
-			TscEthernetDll.sendcommand("TEXT 100,100,\"3\",0,1,1,\"123456\"\r\n");
-			TscEthernetDll.sendcommand("PRINT 1\r\n");
-			TscEthernetDll.closeport(3000);
-			Toast.makeText(this.cordova.getActivity(), "Printed!", Toast.LENGTH_LONG).show();
-*/
 
-			printBmp(address, 9100, message);
+			printBmp(address, message);
 
 			//https://www.tscprinters.com/EN/DownloadFile/readpdf/support/2044/Android_SDK_instruction_E.pdf?file_type=0
 			
 			//https://www.tscprinters.com/EN/DownloadFile/readpdf/support/2044/Android_SDK_instruction_E.pdf?file_type=0
 
 			// https://www.programmersought.com/article/58914782937/
-
-
-			//TscEthernetDll.sendcommand("SIZE 150 mm, 100 mm\r\n");
-
-			//Set the size of the paper (same size as thermal paper) 100 * 150
-			//TscEthernetDll.setup(100, 150, 4, 4, 0, 0, 0);
-			
-			//String path = "/sdcard/Download/" + fileName + IMG_TYPE; 
-			
-			//Set the coordinates of the picture on the label paper
-			//TscEthernetDll.sendpicture(0, 200, message); //path
-			//String status = TscEthernetDll.status();
-
-
-			//TscEthernetDll.downloadbmp(message); // image > message
-			//TscEthernetDll.sendcommand("PUTBMP 100,520,\"Triangle.bmp\"\n");
-
-			//Thread.sleep(1000); //Delay for one second to wait for loading and then print
-
-   			//TscEthernetDll.printlabel(1, 1);//Print out the data in the buffer, the first parameter is the score printed, and the second is the number of sheets printed without a copy
-   			//TscEthernetDll.closeport();
-
-// this is for align from app, beta settings
-//TscEthernetDll.sendcommand("GAP 2 mm, 0 mm\r\n");
-
-			//TscEthernetDll.downloadbmp("Triangle.bmp"); // image > message
-			//TscEthernetDll.sendcommand("PUTBMP 100,520,\"Triangle.bmp\"\n");
-			//String status = TscEthernetDll.printerstatus();
-			//text1.setText(status);
-
-			//Toast.makeText(this.cordova.getActivity(), "Printed!", Toast.LENGTH_LONG).show();
 
 		} else if(action.equals("status")) {
 
